d9572cdd6ac26697724fa828e65933dde1701b3e
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2024-12-09 20:31:44 -0300

    Itens da nota já  podem ser editados e atualizam o valor da nota

--- front/src/app/pages/nota/components/lista-nota/lista-nota.component.ts
+++ front/src/app/pages/nota/components/lista-nota/lista-nota.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NotaService } from '../../services/nota.service';
 import { Nota } from '../../interfaces/Nota';
 import { AppInfoService } from 'src/app/shared/services';
+import { DxDataGridComponent } from 'devextreme-angular';
+import { Cliente } from 'src/app/pages/cliente/interfaces/Cliente';
+import { ClienteService } from 'src/app/pages/cliente/services/cliente.service';
+import { ProdutoService } from 'src/app/pages/produto/services/produto.service';
+import { Produto } from 'src/app/pages/produto/interfaces/Produto';
 
 @Component({
   selector: 'app-lista-nota',
@@ -11,6 +16,17 @@ import { AppInfoService } from 'src/app/shared/services';
 export class ListaNotaComponent implements OnInit {
 
   notas!: Nota[];
+
+  clientes!: Cliente[];
+  clienteSelecionado!: Cliente;
+
+  produtos!: Produto[];
+  produtoSelecionado!: Produto;
+  limpaPdt!: Produto;
+
+  qtdProduto!: number;
+  valorTotalItem!: number;
+  valorTotalAntItem!: number;
 
   get existemDados(): boolean {
     return this.notas && this.notas.length > 0;
@@ -22,13 +38,16 @@ export class ListaNotaComponent implements OnInit {
 
   focoRowId!: number;
 
-  constructor(private notaService: NotaService, private appInfo: AppInfoService) {
+  constructor(private notaService: NotaService, private appInfo: AppInfoService, private clienteService: ClienteService, private produtoService: ProdutoService) {
 
   }
 
   ngOnInit(): void {
     this.appInfo.title = "Notas";
     this.carregaDataGrid();
+
+    this.clienteService.lista().subscribe(result => this.clientes = result);
+    this.produtoService.lista().subscribe(result => this.produtos = result);
   }
 
   carregaDataGrid(): void {
@@ -66,4 +85,87 @@ export class ListaNotaComponent implements OnInit {
     this.exibePopupNota = false;
     this.carregaDataGrid();
   }
+
+  recalculaValorNota(nota: any) {
+
+    const idLinhaFoco = nota.data.id;
+
+    const notaEmFoco = this.notas.find((item) => item.id === idLinhaFoco);
+
+    const valorTot = notaEmFoco!.valorTotal;
+
+    notaEmFoco!.valorTotal = (valorTot - this.valorTotalAntItem) + this.valorTotalItem;
+
+    this.limpaCamposItem();
+  }
+
+  recalculaValorTotItem(rowData: any, origem: string): void {
+
+    if (origem === 'pdt') {
+      this.qtdProduto = rowData['quantidade'];
+    }
+
+    if (origem === 'qtd') {
+      this.produtoSelecionado = rowData['produto'];
+    }
+
+    this.valorTotalAntItem = rowData['valorTotal'];
+
+    if (this.qtdProduto && this.qtdProduto != 0 && this.produtoSelecionado) {
+      this.valorTotalItem = this.produtoSelecionado.preco * this.qtdProduto;
+      console.log('VALOR ITEM RECALCULADO: ', this.valorTotalItem)
+      rowData['valorTotal'] = this.valorTotalItem;
+    }
+  }
+
+  vinculaQtdData(rowData: any, event: any, qtd: any): void {
+    rowData['qtdProduto'] = event.value;
+    this.qtdProduto = event.value;
+    qtd.data.quantidade = event.value;
+
+    this.recalculaValorTotItem(rowData, 'qtd');
+  }
+
+  vinculaPdtData(rowData: any, event: any, selectPdt: any): void {
+
+    if (event.value) {
+      rowData['produto'] = event.value;
+      this.produtoSelecionado = event.value;
+    }
+
+    this.recalculaValorTotItem(rowData, 'pdt');
+  }
+
+  limpaCamposItem(): void {
+
+    this.produtoSelecionado = this.limpaPdt;
+    this.qtdProduto = 0;
+    this.valorTotalItem = 0;
+
+  }
+
+  deletaItem(event: any, nota: any) {
+
+    event.cancel = true;
+
+    console.log('O Item/evento:', event);
+
+    console.log('Valor total item: ', event.data.valorTotal);
+
+    console.log('A nota:  ', nota)
+
+    console.log('Valor total nota: ', nota.data.valorTotal);
+
+
+
+    /*
+    const novoValorNota = nota.data.valorTotal - event.data.valorTotal
+
+    nota.data.valorTotal = novoValorNota;  */
+  }
+
+  test(nota: any): void {
+    console.log('TESTE: ', nota);
+  }
+
 }
